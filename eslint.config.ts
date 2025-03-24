54d50b1399101ce7753f689648850e35646b76b1
Hu Gang <18768366022@163.com>
2025-03-24 16:52:36 +0800

    fix:  configs does not include the overriders field cause error

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,21 +2,20 @@ import { ganghu } from './src'
 
 export default ganghu(
   {
-    typescript: true,
-    vue: true,
-    overrides: {
-      typescript: {
+    typescript: {
+      overrides: {
         'no-console': 'warn',
       },
     },
+    vue: true,
+    // overrides: {
+    //   typescript: {
+    //     'no-console': 'warn',
+    //   },
+    // },
   },
   {
     ignores: ['node_modules', 'dist', 'src/typegen.ts'],
   },
-  // {
-  //   files: ["src/**/*.ts"],
-  //   rules: {
-  //     "no-console": "warn",
-  //   },
-  // }
+
 )
--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,16 @@ import type { Linter } from 'eslint'
 import type { Awaitable, OptionsConfig, TypedFlatConfigItem } from './types'
 import type { Rules } from './typegen'
 
+export type ResolvedOptions<T> = T extends boolean ? never : NonNullable<T>
+export function resolveSubOptions<K extends keyof OptionsConfig>(options: OptionsConfig, key: K): ResolvedOptions<OptionsConfig[K]> {
+  return typeof options[key] === 'boolean' ? {} as any : options[key] || {} as any
+}
+
 export function getOverrides<K extends keyof OptionsConfig>(options: OptionsConfig, key: K): Partial<Linter.RulesRecord & Rules> {
+  const sub = resolveSubOptions(options, key)
   return {
-    ...(options.overrides as any)[key],
+    ...(options.overrides as any)?.[key],
+    ...'overrides' in sub ? sub.overrides : {},
   }
 }
 
